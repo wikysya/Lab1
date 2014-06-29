83e4418c4543ad41660ea10cc5ebb52ff6016fed
wikysya <wika91_19@mail.ru>
2014-06-29 21:16:03 +0400

    initial project version

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(836, 484);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.ZoomEvent += new ZedGraph.ZedGraphControl.ZoomEventHandler(this.zedGraphControl1_ZoomEvent);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(854, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(938, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "b";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(896, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Заново";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(869, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Точность l";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(939, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ε";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.DecimalPlaces = 3;
+            this.numericUpDown2.Location = new System.Drawing.Point(959, 45);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(0);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown2.TabIndex = 12;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Location = new System.Drawing.Point(875, 45);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(0);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown1.TabIndex = 13;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            -2147483648});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown4.DecimalPlaces = 4;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.numericUpDown4.Location = new System.Drawing.Point(959, 97);
+            this.numericUpDown4.Margin = new System.Windows.Forms.Padding(0);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147221504});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown4.TabIndex = 14;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown3.DecimalPlaces = 5;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            262144});
+            this.numericUpDown3.Location = new System.Drawing.Point(959, 71);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(0);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147155968});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(60, 20);
+            this.numericUpDown3.TabIndex = 15;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            262144});
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(896, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 28);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Шаг";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.dihotomiya);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1022, 508);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button3;
+
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private GraphPane myPane;
+        private double a = double.NaN, b = double.NaN, ε, l;
+        public Form1()
+        {
+            InitializeComponent();
+            myPane = zedGraphControl1.GraphPane;
+        }
+
+        public void dihotomiya(object sender, EventArgs e)
+        {
+            #region если это первый шаг то считываем заданные значения
+            if (double.IsNaN(a) || double.IsNaN(b))
+                button2_Click(sender, e);
+            #endregion
+
+            if (Math.Abs(b - a) >= l)
+            {
+                #region удаляем из памяти все линии кроме графика
+                int i = myPane.CurveList.Count - 1;
+                while (i >= 1)
+                {
+                    myPane.CurveList.RemoveAt(i);
+                    i--;
+                }
+                #endregion
+                #region добавляем границы отрезка a и b
+                PointPairList listA = new PointPairList();
+                PointPairList listB = new PointPairList();
+                listA.Add(a, -20);
+                listA.Add(a, 80);
+                listB.Add(b, -20);
+                listB.Add(b, 80);
+                LineItem myCurve = myPane.AddCurve("a", listA, Color.Magenta, SymbolType.None);
+                myCurve = myPane.AddCurve("b", listB, Color.DeepSkyBlue, SymbolType.None);
+                #endregion
+
+                double yk, zk, fyk, fzk;
+                yk = (a + b - ε) / 2;
+                zk = (a + b + ε) / 2;
+                fyk = yk * yk + 3 * yk - 4;  //ФОРМУЛА!!!!!!!!!!!
+                fzk = zk * zk + 3 * zk - 4;  //ФОРМУЛА!!!!!!!!!!!
+                #region добавляем линии Yk и Zk
+                PointPairList listYk = new PointPairList();
+                PointPairList listZk = new PointPairList();
+                listYk.Add(yk, -20);
+                listYk.Add(yk, 80);
+                listZk.Add(zk, -20);
+                listZk.Add(zk, 80);
+                myCurve = myPane.AddCurve("Yk", listYk, Color.ForestGreen, SymbolType.None);
+                myCurve = myPane.AddCurve("Zk", listZk, Color.Lime, SymbolType.None);
+                #endregion
+
+                if (fyk > fzk)
+                {
+                    #region добавляем линии вычеркивающие область
+                    PointPairList list1 = new PointPairList();
+                    PointPairList list2 = new PointPairList();
+                    list1.Add(a, -20);
+                    list1.Add(yk, 80);
+                    list2.Add(a, 80);
+                    list2.Add(yk, -20);
+                    myCurve = myPane.AddCurve("", list1, Color.OrangeRed, SymbolType.None);
+                    myCurve.Line.Width = 2.0F;
+                    myCurve.Label.IsVisible = false;
+                    myCurve = myPane.AddCurve("", list2, Color.OrangeRed, SymbolType.None);
+                    myCurve.Line.Width = 2.0F;
+                    myCurve.Label.IsVisible = false;
+                    #endregion
+                    a = yk;
+                }
+                else
+                {
+                    #region добавляем линии вычеркивающие область
+                    PointPairList list1 = new PointPairList();
+                    PointPairList list2 = new PointPairList();
+                    list1.Add(b, -20);
+                    list1.Add(zk, 80);
+                    list2.Add(b, 80);
+                    list2.Add(zk, -20);
+                    myCurve = myPane.AddCurve("", list1, Color.OrangeRed, SymbolType.None);
+                    myCurve.Line.Width = 2.0F;
+                    myCurve.Label.IsVisible = false;
+                    myCurve = myPane.AddCurve("", list2, Color.OrangeRed, SymbolType.None);
+                    myCurve.Line.Width = 2.0F;
+                    myCurve.Label.IsVisible = false;
+                    #endregion
+                    b = zk;
+                }
+
+                #region перерисовываем график
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Refresh();
+                zedGraphControl1.Visible = true;
+                #endregion
+            }
+            else
+            {
+                double xmin = a + (b - a) / 2;
+                double fmin = xmin * xmin + 3 * xmin - 4;   //ФОРМУЛА!!!!!!!!!!!
+                #region округление значений до заданной точности
+                int c; 
+                for (c = 0; l < 1; c++) //узнаем заданную точность, количество знаков после запятой
+                    l*= 10;
+                
+                fmin =Math.Round(fmin, c);
+                xmin=Math.Round(xmin, c);
+                #endregion
+
+                MessageBox.Show("x="+Convert.ToString(xmin)+"\nFmin="+Convert.ToString(fmin),"Ответ");
+                button3.Enabled = false; //кнопка Шаг недоступна
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreateGraph();
+            SetSize();
+        }
+
+        /// <summary>
+        /// Рисует график функции
+        /// </summary>
+        private void CreateGraph()
+        {
+            myPane.Title.Text = "График функции";
+            myPane.XAxis.Title.Text = "Ось Ox";
+            myPane.YAxis.Title.Text = "Ось Oy";
+            myPane.XAxis.Scale.Min = -10;
+            myPane.XAxis.Scale.Max = 7;
+            myPane.YAxis.Scale.Min = -20;
+            myPane.YAxis.Scale.Max = 80;
+            myPane.Legend.Position = ZedGraph.LegendPos.Right; //расположение легенды
+
+            #region добавляем линии сетки и делаем их серыми
+            myPane.XAxis.MajorGrid.IsVisible = true;
+            myPane.YAxis.MajorGrid.IsVisible = true;
+            myPane.XAxis.MajorGrid.Color = Color.Gray;
+            myPane.YAxis.MajorGrid.Color = Color.Gray;
+            #endregion
+
+            PointPairList list1 = new PointPairList();
+            for (double x = -10; x <= 7; x += 0.0001)
+            {
+                list1.Add(x, x * x + 3 * x - 4);   //ФОРМУЛА!!!!!!!!!!!
+            }
+            LineItem myCurve = myPane.AddCurve("y = x^2 + 3x - 4", list1, Color.Blue, SymbolType.None);
+            myCurve.Line.Width = 2.0F;
+            zedGraphControl1.AxisChange();
+        }
+
+        private void SetSize()
+        {
+            zedGraphControl1.Location = new Point(10, 10);
+            zedGraphControl1.Size = new Size(ClientRectangle.Width - 180, ClientRectangle.Height - 20);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            SetSize();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(this.numericUpDown1.Value);
+            b = Convert.ToDouble(this.numericUpDown2.Value);
+            ε = Convert.ToDouble(this.numericUpDown3.Value);
+            l = Convert.ToDouble(this.numericUpDown4.Value);
+            button3.Enabled = true;
+        }
+
+        /// <summary>
+        /// Перерисовывает линии при зуммировании
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="oldState"></param>
+        /// <param name="newState"></param>
+        private void zedGraphControl1_ZoomEvent(ZedGraphControl sender, ZoomState oldState, ZoomState newState)
+        {
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Refresh();
+            zedGraphControl1.Visible = true;
+        }
+    }
+}
